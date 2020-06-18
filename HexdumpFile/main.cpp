6c5cc01cf07cadbb0eade0e790681e61f03ec0d7
hugsy <hugsy@blah.cat>
2020-06-18 08:43:06 -0700

    [hexdumpfile] done

--- HexdumpFile/main.cpp
+++ HexdumpFile/main.cpp
@@ -2,11 +2,12 @@
 #pragma comment(lib, "../x64/release/pwn++.lib")
 
 using namespace pwn::log;
-
 namespace ctx = pwn::context;
 
+#include <vector>
 
-auto wmain(_In_ int argc, _In_ const wchar_t** argv)
+
+auto wmain(_In_ int argc, _In_ const wchar_t** argv) -> int
 {
 	if (argc < 2)
 	{
@@ -14,7 +15,15 @@ auto wmain(_In_ int argc, _In_ const wchar_t** argv)
 		return EXIT_FAILURE;
 	}
 
-
+	auto h = pwn::generic::GenericHandle(::CreateFile(argv[1], GENERIC_READ, 0, nullptr, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr));
+	if (h)
+	{
+		auto sz = ::GetFileSize(h.Get(), nullptr);
+		std::vector<BYTE> bytes(sz);
+		DWORD dummy;
+		if (::ReadFile(h.Get(), bytes.data(), sz, &dummy, nullptr))
+			pwn::utils::hexdump(bytes);
+	}
 
 	return EXIT_SUCCESS;
 }
