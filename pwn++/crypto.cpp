bdc308a214d5ce5e956d0112d6772f973b6af469
hugsy <hugsy@blah.cat>
2020-12-09 17:29:30 -0800

    [pwn::crypto] added tests for crypto

--- pwn++/crypto.cpp
+++ pwn++/crypto.cpp
@@ -5,12 +5,11 @@
 
 
 /**
-* 
-* Terrible CRC function implemention, just packaged here for convienence. Don't use for real code, use Boost.
-* 
-* https://en.wikipedia.org/wiki/Cyclic_redundancy_check
-* 
-*/
+ * 
+ * Those are NOT valid CRC implementations!! Use https://en.wikipedia.org/wiki/Cyclic_redundancy_check for that
+ * I used those for fast hashing, not for redundancy check.
+ * 
+ */
 
 
 template <typename T>
@@ -18,13 +17,12 @@ static T __crc_base(std::vector<BYTE> const& data, T variant)
 {
 	auto n = data.size();
 	T crc = -1;
-
-	for (size_t i = 0; i < n; i++)
+	for (auto i = 0; i < n; i++)
 	{
 		auto c = data.at(i);
 		T b = (T)c;
 
-		for (u8 j = 0; j < 8; j++)
+		for (auto j = 0; j < 8; j++)
 		{
 			b = (b ^ crc) & 1;
 			crc >>= 1;
@@ -40,47 +38,57 @@ static T __crc_base(std::vector<BYTE> const& data, T variant)
 
 BYTE pwn::crypto::crc8(std::vector<BYTE> const& data)
 {
-	return __crc_base<BYTE>(data, (BYTE)Crc8Variant::Crc8Bluetooth);
+	return __crc_base<BYTE>(data, 0xE5);
 }
 
 
 WORD pwn::crypto::crc16(std::vector<BYTE> const& data)
 {
-	return __crc_base<WORD>(data, (WORD)Crc16Variant::Crc16Ccitt);
+	return __crc_base<WORD>(data, 0x8408);
 }
 
 
 DWORD pwn::crypto::crc32(std::vector<BYTE> const& data)
 {
-	return __crc_base<DWORD>(data, (DWORD)Crc32Variant::Crc32);
+	return __crc_base<DWORD>(data, 0xEDB88320);
 }
 
 
 DWORD64 pwn::crypto::crc64(std::vector<BYTE> const& data)
 {
-	return __crc_base<DWORD64>(data, (DWORD64)Crc64Variant::Crc64Ecma);
+	return __crc_base<DWORD64>(data, 0xC96C5795D7870F42);
 }
 
 
 // todo: implem sha1, sha256, sha512 with cryptoapi 
-
-std::array<BYTE, MD5LEN> __calc_hash_md(std::vector<BYTE> const& data, DWORD dwMdType)
+template <typename T>
+static T __calc_hash(std::vector<BYTE> const& data, DWORD dwHashAlgVariant, DWORD dwProvider)
 {
-	std::array<BYTE, MD5LEN> out = {};
+	T out = {};
 	auto n = data.size();
 	HCRYPTPROV hProv = 0;
 	HCRYPTHASH hHash = 0;
-	DWORD cbHash = MD5LEN;
+	DWORD cbHash = out.size();
 
-	CryptAcquireContext(&hProv, nullptr, nullptr, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT);
-	CryptCreateHash(hProv, dwMdType, 0, 0, &hHash);
-	CryptHashData(hHash, data.data(), n, 0);
-	CryptGetHashParam(hHash, HP_HASHVAL, &out[0], &cbHash, 0);
-	CryptDestroyHash(hHash);
-	CryptReleaseContext(hProv, 0);
+
+	if (CryptAcquireContext(&hProv, nullptr, nullptr, dwProvider, CRYPT_VERIFYCONTEXT))
+	{
+		if (CryptCreateHash(hProv, dwHashAlgVariant, 0, 0, &hHash))
+		{
+			if (CryptHashData(hHash, data.data(), n, 0))
+			{
+				CryptGetHashParam(hHash, HP_HASHVAL, &out[0], &cbHash, 0);
+			}
+			CryptDestroyHash(hHash);
+		}
+		CryptReleaseContext(hProv, 0);
+	}
 	return out;
 }
 
-std::array<BYTE, MD5LEN> pwn::crypto::md2(std::vector<BYTE> const& data) {	return __calc_hash_md(data, CALG_MD2); }
-std::array<BYTE, MD5LEN> pwn::crypto::md4(std::vector<BYTE> const& data) {	return __calc_hash_md(data, CALG_MD4); }
-std::array<BYTE, MD5LEN> pwn::crypto::md5(std::vector<BYTE> const& data) {	return __calc_hash_md(data, CALG_MD5); }
+std::array<BYTE, MD5LEN> pwn::crypto::md2(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, MD5LEN>>(data, CALG_MD2, PROV_RSA_FULL);}
+std::array<BYTE, MD5LEN> pwn::crypto::md4(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, MD5LEN>>(data, CALG_MD4, PROV_RSA_FULL);}
+std::array<BYTE, MD5LEN> pwn::crypto::md5(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, MD5LEN>>(data, CALG_MD5, PROV_RSA_FULL);}
+std::array<BYTE, SHA1LEN> pwn::crypto::sha1(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, SHA1LEN>>(data, CALG_SHA1, PROV_RSA_FULL);}
+std::array<BYTE, SHA256LEN> pwn::crypto::sha256(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, SHA256LEN>>(data, CALG_SHA_256, PROV_RSA_AES);}
+std::array<BYTE, SHA512LEN> pwn::crypto::sha512(std::vector<BYTE> const& data) {return __calc_hash<std::array<BYTE, SHA512LEN>>(data, CALG_SHA_512, PROV_RSA_AES);}
--- pwn++/crypto.h
+++ pwn++/crypto.h
@@ -4,37 +4,16 @@
 
 
 #define MD5LEN  16
+#define SHA1LEN  20
+#define SHA256LEN  32
+#define SHA512LEN  64
 
 
 namespace pwn::crypto
 {
 	/**
-	 * CRC 
+	 * CRCx
 	 */
-	enum class Crc8Variant : BYTE
-	{
-		Crc8Bluetooth = 0xE5,
-	};
-
-	enum class Crc16Variant : WORD
-	{
-		Crc16Ccitt = 0x8408,
-	};
-
-	enum class Crc32Variant : DWORD
-	{
-		Crc32 = 0xEDB88320,
-		Crc32Castagnoli = 0x82F63B78,
-		Crc32Koopman = 0xEB31D82E,
-		Crc32Koopman2 = 0x992C1A4C
-	};
-
-	enum class Crc64Variant : DWORD64
-	{
-		Crc64Iso = 0xD800000000000000,
-		Crc64Ecma = 0xC96C5795D7870F42
-	};
-
 	PWNAPI BYTE crc8(std::vector<BYTE> const&);
 	PWNAPI WORD crc16(std::vector<BYTE> const&);
 	PWNAPI DWORD crc32(std::vector<BYTE> const&);
@@ -42,7 +21,7 @@ namespace pwn::crypto
 
 
 	/**
-	 * MD
+	 * MDx
 	 */
 
 	PWNAPI std::array<BYTE, MD5LEN> md2(std::vector<BYTE> const& data);
@@ -52,6 +31,9 @@ namespace pwn::crypto
 
 
 	/**
-	 * SHA
+	 * SHAx
 	 */
+	PWNAPI std::array<BYTE, SHA1LEN> sha1(std::vector<BYTE> const& data);
+	PWNAPI std::array<BYTE, SHA256LEN> sha256(std::vector<BYTE> const& data);
+	PWNAPI std::array<BYTE, SHA512LEN> sha512(std::vector<BYTE> const& data);
 }
