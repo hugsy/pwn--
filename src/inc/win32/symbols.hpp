c3e050d4cefc1b50424ec8ba51131e1f9a23594f
hugsy <hugsy@blah.cat>
2022-10-13 11:15:32 -0700

    [pwn::win] made symbols non static

--- src/inc/win32/symbols.hpp
+++ src/inc/win32/symbols.hpp
@@ -2,26 +2,61 @@
 
 // clang-format off
 #include "common.hpp"
-
-#include <DbgHelp.h>
 // clang-format on
 
 
 namespace pwn::windows
 {
 
+struct SymbolInfo
+{
+    u32 Type;
+    u32 Size;
+    u64 ModBase;
+    u32 Flags;
+    uptr Value;
+    uptr Address;
+    u32 Register;
+    std::wstring Name;
+};
+
 class Symbols
 {
 public:
+    ///
+    ///@brief
+    ///
+    ///@return Result<std::vector<std::tuple<uptr, std::wstring>>>
+    ///
     static Result<std::vector<std::tuple<uptr, std::wstring>>>
     EnumerateModules();
 
-    static Result<std::vector<SYMBOL_INFO>>
+    ///
+    ///@brief
+    ///
+    ///@param ModuleName
+    ///@param Mask
+    ///@return Result<std::vector<SymbolInfo>>
+    ///
+    static Result<std::vector<SymbolInfo>>
     EnumerateFromModule(std::wstring_view const& ModuleName, std::wstring_view const& Mask = L"*");
 
+    ///
+    ///@brief
+    ///
+    ///@param TargetAddress
+    ///@param SymbolName
+    ///@return Result<std::wstring>
+    ///
     static Result<std::wstring>
     ResolveFromName(const uptr TargetAddress, std::wstring_view const& SymbolName);
 
+    ///
+    ///@brief
+    ///
+    ///@param TargetAddress
+    ///@return Result<std::wstring>
+    ///
     static Result<std::wstring>
     ResolveFromAddress(const uptr TargetAddress);
 };
--- src/pwn++/win32/symbols.cpp
+++ src/pwn++/win32/symbols.cpp
@@ -2,30 +2,133 @@
 
 #include "log.hpp"
 
-// TODO: make it dynamic?
-#pragma comment(lib, "Dbghelp.lib")
-
 static HANDLE __hProcess = nullptr;
+static HMODULE __hModule = nullptr;
+
+#pragma region Declaration
+struct SYMBOL_INFOW
+{
+    ULONG SizeOfStruct;
+    ULONG TypeIndex; // Type Index of symbol
+    ULONG64 Reserved[2];
+    ULONG Index;
+    ULONG Size;
+    ULONG64 ModBase; // Base Address of module comtaining this symbol
+    ULONG Flags;
+    ULONG64 Value;   // Value of symbol, ValuePresent should be 1
+    ULONG64 Address; // Address of symbol including base address of module
+    ULONG Register;  // register holding value or pointer to value
+    ULONG Scope;     // scope of the symbol
+    ULONG Tag;       // pdb classification
+    ULONG NameLen;   // Actual length of name
+    ULONG MaxNameLen;
+    WCHAR Name[1]; // Name of symbol
+};
+
+
+typedef DWORD (*SymSetOptions_t)(DWORD SymOptions);
+static SymSetOptions_t SymSetOptions = nullptr;
+
+typedef BOOL (*SymInitializeW_t)(HANDLE hProcess, PWSTR UserSearchPath, BOOL fInvadeProcess);
+static SymInitializeW_t SymInitializeW = nullptr;
+
+typedef BOOL (*SymEnumerateModulesW64_t)(HANDLE hProcess, PVOID EnumModulesCallback, PVOID UserContext);
+static SymEnumerateModulesW64_t SymEnumerateModulesW64 = nullptr;
+
+typedef ULONG_PTR (*SymLoadModuleExW_t)(
+    HANDLE hProcess,
+    HANDLE hFile,
+    PCWSTR ImageName,
+    PCWSTR ModuleName,
+    DWORD64 BaseOfDll,
+    DWORD DllSize,
+    PVOID ModLoadData,
+    DWORD Flags);
+static SymLoadModuleExW_t SymLoadModuleExW = nullptr;
+
+typedef BOOL (
+    *SymEnumSymbolsW_t)(HANDLE hProcess, ULONG64 BaseOfDll, PCWSTR Mask, PVOID EnumSymbolsCallback, PVOID UserContext);
+static SymEnumSymbolsW_t SymEnumSymbolsW = nullptr;
+
+typedef BOOL (*SymFromNameW_t)(HANDLE hProcess, PCWSTR Name, SYMBOL_INFOW* Symbol);
+static SymFromNameW_t SymFromNameW = nullptr;
+
+typedef BOOL (*SymFromAddrW_t)(HANDLE hProcess, uptr Address, uptr* Displacement, SYMBOL_INFOW* Symbol);
+static SymFromAddrW_t SymFromAddrW = nullptr;
+
+
+#ifndef SYMOPT_CASE_INSENSITIVE
+#define SYMOPT_CASE_INSENSITIVE 0x00000001
+#endif // SYMOPT_CASE_INSENSITIVE
+
+#ifndef SYMOPT_UNDNAME
+#define SYMOPT_UNDNAME 0x00000002
+#endif // SYMOPT_UNDNAME
+
+#ifndef SYMOPT_DEFERRED_LOADS
+#define SYMOPT_DEFERRED_LOADS 0x00000004
+#endif // SYMOPT_DEFERRED_LOADS
+
+#ifndef MAX_SYM_NAME
+#define MAX_SYM_NAME 2000
+#endif // MAX_SYM_NAME
+
+#pragma endregion Declaration
+
 
 namespace pwn::windows
 {
 
+SymbolInfo
+CreateSymbolInfo(SYMBOL_INFOW const* _si)
+{
+    SymbolInfo si {};
+    si.Type     = _si->TypeIndex;
+    si.Size     = _si->Size;
+    si.ModBase  = _si->ModBase;
+    si.Flags    = _si->Flags;
+    si.Value    = _si->Value;
+    si.Address  = _si->Address;
+    si.Register = _si->Register;
+    si.Name     = _si->Name;
+    return si;
+}
+
 inline HANDLE
 GetHandle()
 {
-    if ( !__hProcess )
+    if ( !__hModule )
     {
-        ::SymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS);
-
-        __hProcess = GetCurrentProcess(); // TODO: adjust
-
-        if ( ::SymInitialize(__hProcess, nullptr, true) == FALSE )
+        __hModule = LoadLibraryW(L"DbgHelp.dll");
+        if ( __hModule && !__hProcess )
         {
-            log::perror(L"SymInitialize()");
-            return nullptr;
+
+            // Initialize the function pointers
+            {
+                SymSetOptions  = (SymSetOptions_t)::GetProcAddress(__hModule, "SymSetOptions");
+                SymInitializeW = (SymInitializeW_t)::GetProcAddress(__hModule, "SymInitializeW");
+                SymEnumerateModulesW64 =
+                    (SymEnumerateModulesW64_t)::GetProcAddress(__hModule, "SymEnumerateModulesW64");
+                SymLoadModuleExW = (SymLoadModuleExW_t)::GetProcAddress(__hModule, "SymLoadModuleExW");
+                SymEnumSymbolsW  = (SymEnumSymbolsW_t)::GetProcAddress(__hModule, "SymEnumSymbolsW");
+                SymFromNameW     = (SymFromNameW_t)::GetProcAddress(__hModule, "SymFromNameW");
+                SymFromAddrW     = (SymFromAddrW_t)::GetProcAddress(__hModule, "SymFromAddrW");
+            }
+
+            // Initialize the debug engine for the target process
+            {
+                ::SymSetOptions(SYMOPT_UNDNAME | SYMOPT_DEFERRED_LOADS);
+
+                __hProcess = ::GetCurrentProcess(); // TODO: adjust
+
+                if ( SymInitializeW(__hProcess, nullptr, true) == FALSE )
+                {
+                    log::perror(L"SymInitialize()");
+                    return nullptr;
+                }
+            }
         }
     }
-
     return __hProcess;
 }
 
@@ -35,7 +138,7 @@ Symbols::EnumerateModules()
     HANDLE hProcess = GetHandle();
     if ( !hProcess )
     {
-        return Err(ErrorCode::InitializationFailed);
+        return Err(ErrorCode::NotInitialized);
     }
     std::vector<std::tuple<uptr, std::wstring>> Modules;
 
@@ -46,7 +149,7 @@ Symbols::EnumerateModules()
         return TRUE;
     };
 
-    if ( ::SymEnumerateModulesW64(hProcess, (PSYM_ENUMMODULES_CALLBACKW64)&EnumModulesCb, nullptr) == FALSE )
+    if ( SymEnumerateModulesW64(hProcess, &EnumModulesCb, nullptr) == FALSE )
     {
         log::perror(L"SymEnumerateModulesW64()");
         return Err(ErrorCode::ExternalApiCallFailed);
@@ -56,32 +159,31 @@ Symbols::EnumerateModules()
 }
 
 
-Result<std::vector<SYMBOL_INFO>>
+Result<std::vector<SymbolInfo>>
 Symbols::EnumerateFromModule(std::wstring_view const& ModuleName, std::wstring_view const& Mask)
 {
     HANDLE hProcess = GetHandle();
     if ( !hProcess )
     {
-        return Err(ErrorCode::InitializationFailed);
+        return Err(ErrorCode::NotInitialized);
     }
 
-    std::vector<SYMBOL_INFO> ModuleSymbolInfo;
-    auto EnumSymProc = [&ModuleSymbolInfo](PSYMBOL_INFO pSymInfo, ULONG SymbolSize, PVOID UserContext) -> bool
+    std::vector<SymbolInfo> ModuleSymbolInfo;
+    auto EnumSymProc = [&ModuleSymbolInfo](SYMBOL_INFOW* pSymInfo, ULONG SymbolSize, PVOID UserContext) -> bool
     {
         UnreferencedParameter(UserContext);
-        ModuleSymbolInfo.push_back(*pSymInfo);
+        ModuleSymbolInfo.push_back(CreateSymbolInfo(pSymInfo));
         return true;
     };
 
-    u64 BaseOfDll = ::SymLoadModuleExW(hProcess, nullptr, ModuleName.data(), nullptr, 0, 0, nullptr, 0);
+    u64 BaseOfDll = SymLoadModuleExW(hProcess, nullptr, ModuleName.data(), nullptr, 0, 0, nullptr, 0);
     if ( !BaseOfDll )
     {
         log::perror(L"SymLoadModuleExW()");
         return Err(ErrorCode::ExternalApiCallFailed);
     }
 
-    if ( ::SymEnumSymbolsW(hProcess, BaseOfDll, Mask.data(), (PSYM_ENUMERATESYMBOLS_CALLBACKW)&EnumSymProc, nullptr) ==
-         FALSE )
+    if ( ::SymEnumSymbolsW(hProcess, BaseOfDll, Mask.data(), &EnumSymProc, nullptr) == FALSE )
     {
         log::perror(L"SymEnumSymbolsW()");
         return Err(ErrorCode::ExternalApiCallFailed);
@@ -90,17 +192,18 @@ Symbols::EnumerateFromModule(std::wstring_view const& ModuleName, std::wstring_v
     return Ok(ModuleSymbolInfo);
 }
 
+
 Result<std::wstring>
 Symbols::ResolveFromName(const uptr TargetAddress, std::wstring_view const& SymbolName)
 {
     HANDLE hProcess = GetHandle();
     if ( !hProcess )
     {
-        return Err(ErrorCode::InitializationFailed);
+        return Err(ErrorCode::NotInitialized);
     }
 
     auto pBuffer          = std::make_unique<u8[]>(sizeof(SYMBOL_INFOW) + MAX_SYM_NAME * sizeof(u16));
-    auto pSymbol          = reinterpret_cast<PSYMBOL_INFOW>(pBuffer.get());
+    auto pSymbol          = reinterpret_cast<SYMBOL_INFOW*>(pBuffer.get());
     pSymbol->SizeOfStruct = sizeof(SYMBOL_INFOW);
     pSymbol->MaxNameLen   = MAX_SYM_NAME;
 
@@ -113,18 +216,18 @@ Symbols::ResolveFromName(const uptr TargetAddress, std::wstring_view const& Symb
     return Ok(std::wstring {pSymbol->Name});
 }
 
+
 Result<std::wstring>
 Symbols::ResolveFromAddress(const uptr TargetAddress)
 {
-
     HANDLE hProcess = GetHandle();
     if ( !hProcess )
     {
-        return Err(ErrorCode::InitializationFailed);
+        return Err(ErrorCode::NotInitialized);
     }
 
     auto pBuffer          = std::make_unique<u8[]>(sizeof(SYMBOL_INFOW) + MAX_SYM_NAME * sizeof(u16));
-    auto pSymbol          = reinterpret_cast<PSYMBOL_INFOW>(pBuffer.get());
+    auto pSymbol          = reinterpret_cast<SYMBOL_INFOW*>(pBuffer.get());
     pSymbol->SizeOfStruct = sizeof(SYMBOL_INFOW);
     pSymbol->MaxNameLen   = MAX_SYM_NAME;
 
