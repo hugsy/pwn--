cc43bb4a3b8a145d28bd39d7d80dec4a961e2ff8
hugsy <crazy.hugsy@outlook.com>
2023-11-08 09:30:27 -0800

    FetchContent(ArgParse) builds

--- cmake/FindArgparse.cmake
+++ cmake/FindArgparse.cmake
@@ -7,3 +7,9 @@ FetchContent_Declare(
 )
 
 FetchContent_MakeAvailable(Deps_Argparse)
+
+message(STATUS "Using ArgParse in '${deps_argparse_SOURCE_DIR}'")
+
+add_library(Deps_ArgParse INTERFACE EXCLUDE_FROM_ALL)
+target_compile_features(Deps_ArgParse INTERFACE cxx_std_17)
+target_include_directories(Deps_ArgParse INTERFACE ${deps_argparse_SOURCE_DIR}/include)
