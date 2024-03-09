952bf012b1d4c817b5ef6373e3a892eff795b03a
hugsy <crazy.hugsy@outlook.com>
2024-03-08 17:02:03 -0800

    avoid mklib to be rebuilt every time

--- cmake/FindMkCLib.cmake
+++ cmake/FindMkCLib.cmake
@@ -21,43 +21,87 @@ if(NOT M4_EXECUTABLE)
     message(FATAL_ERROR "m4 macro processor not found")
 endif()
 
-add_custom_target(
-    Deps_Mk_Clib_Crypto_M4PreBuild
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lang_bui_inl_defd.h.m4                  > mk_lang_bui_inl_defd.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lang_bui_inl_defu.h.m4                  > mk_lang_bui_inl_defu.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lang_bui_inl_filec.h.m4                 > mk_lang_bui_inl_filec.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lang_bui_inl_fileh.h.m4                 > mk_lang_bui_inl_fileh.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_sl_cui_inl_defd.h.m4                    > mk_sl_cui_inl_defd.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_sl_cui_inl_defu.h.m4                    > mk_sl_cui_inl_defu.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_sl_cui_inl_filec.h.m4                   > mk_sl_cui_inl_filec.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_sl_cui_inl_fileh.h.m4                   > mk_sl_cui_inl_fileh.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_alg_aes_128.h.m4             > mk_lib_crypto_alg_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_alg_aes_192.h.m4             > mk_lib_crypto_alg_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_alg_aes_256.h.m4             > mk_lib_crypto_alg_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cbc_aes_128.h.m4        > mk_lib_crypto_mode_cbc_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cbc_aes_192.h.m4        > mk_lib_crypto_mode_cbc_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cbc_aes_256.h.m4        > mk_lib_crypto_mode_cbc_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cbc_serpent.h.m4        > mk_lib_crypto_mode_cbc_serpent.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_128_aes_128.h.m4    > mk_lib_crypto_mode_cfb_128_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_128_aes_192.h.m4    > mk_lib_crypto_mode_cfb_128_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_128_aes_256.h.m4    > mk_lib_crypto_mode_cfb_128_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_128_serpent.h.m4    > mk_lib_crypto_mode_cfb_128_serpent.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_8_aes_128.h.m4      > mk_lib_crypto_mode_cfb_8_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_8_aes_192.h.m4      > mk_lib_crypto_mode_cfb_8_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_8_aes_256.h.m4      > mk_lib_crypto_mode_cfb_8_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_cfb_8_serpent.h.m4      > mk_lib_crypto_mode_cfb_8_serpent.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ctr_be_aes_128.h.m4     > mk_lib_crypto_mode_ctr_be_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ctr_be_aes_192.h.m4     > mk_lib_crypto_mode_ctr_be_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ctr_be_aes_256.h.m4     > mk_lib_crypto_mode_ctr_be_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ctr_be_serpent.h.m4     > mk_lib_crypto_mode_ctr_be_serpent.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ecb_aes_128.h.m4        > mk_lib_crypto_mode_ecb_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ecb_aes_192.h.m4        > mk_lib_crypto_mode_ecb_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ecb_aes_256.h.m4        > mk_lib_crypto_mode_ecb_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ecb_serpent.h.m4        > mk_lib_crypto_mode_ecb_serpent.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ofb_aes_128.h.m4        > mk_lib_crypto_mode_ofb_aes_128.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ofb_aes_192.h.m4        > mk_lib_crypto_mode_ofb_aes_192.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ofb_aes_256.h.m4        > mk_lib_crypto_mode_ofb_aes_256.h
-    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} mk_lib_crypto_mode_ofb_serpent.h.m4        > mk_lib_crypto_mode_ofb_serpent.h
+set(MK_CLIB_SOURCE_M4_H
+    ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defd.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defu.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_filec.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_fileh.h
+    ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defd.h
+    ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defu.h
+    ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_filec.h
+    ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_fileh.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_serpent.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_serpent.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_serpent.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_serpent.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_serpent.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_128.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_192.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_256.h
+    ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_serpent.h
+)
+
+# add_custom_target(
+#     Deps_Mk_Clib_Crypto_M4PreBuild
+#     WORKING_DIRECTORY ${MK_CLIB_SOURCE_DIR}
+#     DEPENDS ${MK_CLIB_SOURCE_M4_H}
+# )
+
+add_custom_command(
+    OUTPUT ${MK_CLIB_SOURCE_M4_H}
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defd.h.m4                  > ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defd.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defu.h.m4                  > ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_defu.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_filec.h.m4                 > ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_filec.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_fileh.h.m4                 > ${MK_CLIB_SOURCE_DIR}/mk_lang_bui_inl_fileh.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defd.h.m4                    > ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defd.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defu.h.m4                    > ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_defu.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_filec.h.m4                   > ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_filec.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_fileh.h.m4                   > ${MK_CLIB_SOURCE_DIR}/mk_sl_cui_inl_fileh.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_128.h.m4             > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_192.h.m4             > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_256.h.m4             > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_alg_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_128.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_192.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_256.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_serpent.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cbc_serpent.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_128.h.m4    > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_192.h.m4    > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_256.h.m4    > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_serpent.h.m4    > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_128_serpent.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_128.h.m4      > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_192.h.m4      > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_256.h.m4      > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_serpent.h.m4      > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_cfb_8_serpent.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_128.h.m4     > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_192.h.m4     > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_256.h.m4     > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_serpent.h.m4     > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ctr_be_serpent.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_128.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_192.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_256.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_serpent.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ecb_serpent.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_128.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_128.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_192.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_192.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_256.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_aes_256.h
+    COMMAND ${M4_EXECUTABLE} -Q -I ${MK_CLIB_SOURCE_DIR} ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_serpent.h.m4        > ${MK_CLIB_SOURCE_DIR}/mk_lib_crypto_mode_ofb_serpent.h
     WORKING_DIRECTORY ${MK_CLIB_SOURCE_DIR}
 )
 
@@ -293,7 +337,7 @@ if(WIN32)
     )
 endif(WIN32)
 
-add_library(Deps_Mk_Clib_Crypto STATIC ${MK_CLIB_SOURCE_FILES})
+add_library(Deps_Mk_Clib_Crypto STATIC ${MK_CLIB_SOURCE_FILES} ${MK_CLIB_SOURCE_M4_H})
 target_include_directories(Deps_Mk_Clib_Crypto PUBLIC ${MK_CLIB_SOURCE_DIR})
 set_property(TARGET Deps_Mk_Clib_Crypto PROPERTY CXX_STANDARD 23)
 target_compile_definitions(
@@ -302,7 +346,7 @@ target_compile_definitions(
     PRIVATE $<$<NOT:$<CONFIG:Debug>>:NDEBUG>
 )
 target_compile_features(Deps_Mk_Clib_Crypto PUBLIC cxx_std_23)
-add_dependencies(Deps_Mk_Clib_Crypto Deps_Mk_Clib_Crypto_M4PreBuild)
+# add_dependencies(Deps_Mk_Clib_Crypto Deps_Mk_Clib_Crypto_M4PreBuild)
 add_library(PWN::Deps::MkClib::Crypto ALIAS Deps_Mk_Clib_Crypto)
 
 #
