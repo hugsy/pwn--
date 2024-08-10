6eae818d9d6d17e78172f1bdd3afcadf7f8e7ac6
hugsy <crazy.hugsy@outlook.com>
2024-08-10 10:45:01 -0700

    starting pwn.ixx module, disabled everything but common

--- Modules/Common/Include/Common.ixx
+++
@@ -1,4 +0,0 @@
-export module common;
-
-export void
-test();
--- Modules/Common/Source/Common.cxx
+++ Modules/Common/Source/Common.cxx
@@ -1,8 +1,24 @@
 module;
 #include <iostream>
-module common;
+export module pwn.common;
 
-void
+export using u8  = std::uint8_t;
+export using u16 = std::uint16_t;
+export using u32 = std::uint32_t;
+export using u64 = std::uint64_t;
+export using i8  = std::int8_t;
+export using i16 = std::int16_t;
+export using i32 = std::int32_t;
+export using i64 = std::int64_t;
+#ifdef _M_IX86
+export using usize = unsigned long;
+#else
+export using usize = std::size_t;
+#endif
+export using ssize = std::intptr_t;
+export using uptr  = std::uintptr_t;
+
+export void
 test()
 {
     std::cout << "test modules\n";
